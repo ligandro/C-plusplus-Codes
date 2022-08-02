6f020f6577f92369f302ba6d3138c88dd913f8e4
Ligandro Singh Yumnam <97714265+ligandro@users.noreply.github.com>
2022-08-02 08:19:14 +0530

    Add files via upload

---
+++ main 2.cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "light and dark" << endl;
+    cout << "   /|" << endl;
+    cout << "light and dark";
+    cout << "   /|" << endl;
+    cout << "light and dark\n";
+    cout << "   /|" << endl;
+    string ph = "Girrafe" ;
+    cout << ph << endl ;
+    cout << ph.length() ;
+    ph[2] = 'R';
+    cout << ph[2] << endl ;
+    cout << ph.find("raf",0) << endl;
+    cout << ph.substr(1,3) << endl;
+    string phs = ph.substr(3,4);
+    cout << phs << endl;
+    
+  
+  
+    
+    
+    
+    
+    return 0;
+}
+
---
+++ main.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello world" << endl;
+    cout << "   /|" << endl;
+    cout << "  / |" << endl;
+    cout << " /  |" << endl;
+    cout << "/___|" << endl;
+    string name = "John";
+    int age = 50;
+    cout << "There was a guy named " << name << endl ;
+    cout << "He was " << age << " years old " << endl;
+    cout << "He really liked the name "<< name <<endl;
+    cout << "But did not like being " << age << endl;
+    
+    char grade = 'A';
+    string nme = " Hu";
+    int height = 67;
+    float x = 2.2;
+    double y = 2.2;
+    bool ismale = true;
+    cout << ismale<< endl;
+    cout << 7.0 << endl;
+    
+    
+    
+    
+    return 0;
+}
+
